69999eb96b6cd21bd8f4939beb563184405bfb73
lucas <lucas@opentown.cn>
2018-09-11 18:30:48 +0800

    init muse project, Muse: music analyze and synthesize

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <key></key>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Key from './components/Key'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+    Key
   }
 }
 </script>
---
+++ src/components/Key.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class='m-key'>
+        <div class='m-note'> Note</div>
+    </div>
+
+</template>
+
+<script>
+    PITCHES = ['A', 'a', 'B', 'C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g']
+    OCTAVED = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    export default {
+        name: 'Key',
+        date: function () {
+            return {
+                pitch: 0,
+                octave: 0,
+                offset: 0,
+                freqency: 0
+            }
+        },
+        props: {
+            note: '',
+            pressed: false
+
+        }
+    }
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .m-key {
+        width: 2em;
+        height: 6em;
+        background-color: red;
+    }
+
+    .m-note {
+        bottom: 0;
+    }
+
+
+
+    .m-key-pressed {
+        background-color: black
+    }
+</style>
